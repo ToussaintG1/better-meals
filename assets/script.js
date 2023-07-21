3dd986f1e13bd17433ee5d76cfe2c0e36afb5472
JaeBEE <johnnae908@yahoo.com>
2023-07-20 20:00:10 -0700

    active api

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,9 @@ var nutritionAPIKey = "95e7d0fbaa2c3f8adb4e92d058479d8e"
 var recipeAPIKey = "ee23ef98a6daf89098efa5d42c0e32fc"
 
 var nutritionURL = "https://trackapi.nutritionix.com/v2/natural/nutrients"
-var recipeURL = "https://api.edamam.com/api/recipes/v2"
+var recipeURL = "https://api.edamam.com/api/recipes/v2";
+
+var x = document.getElementById("input-element");
 
 // Nutritionix API ID: 7f8298cc
 
@@ -18,7 +20,30 @@ fetch(nutritionURL, {
 
     },
     body: JSON.stringify({
-        "query": "for breakfast i ate 2 eggs, bacon, and french toast",
+        //"query": "for breakfast i ate 2 eggs, bacon, and french toast",
+        "query": x.value,
+        "timezone": "US/Eastern"
+    })
+})
+.then(function(response){
+    console.log (response)
+    return response.json()
+})
+.then(function(data){
+    console.log (data)
+}) 
+
+fetch(recipeURL, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "x-app-id":"afa2c155", 
+        "x-app-key": recipeAPIKey
+
+    },
+    body: JSON.stringify({
+        //"query": "for breakfast i ate 2 eggs, bacon, and french toast",
+        "query": x.value,
         "timezone": "US/Eastern"
     })
 })
